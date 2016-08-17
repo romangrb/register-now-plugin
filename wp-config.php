9c67dd92bfddbf5d4eeea338cf2cb4c7087bacc8
Roman <grabarroman@gmail.com>
2016-08-17 16:06:16 +0000

    debug enabled

--- wp-config.php
+++ wp-config.php
@@ -78,9 +78,22 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+
 $_SERVER["HTTP_HOST"] = $_SERVER["SERVER_NAME"];
 $_SERVER["HTTP_HOST"] = $_SERVER["SERVER_NAME"];
+
+ // Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings 
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set( 'display_errors', 0 );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', true );
 
 /* That's all, stop editing! Happy blogging. */
 
