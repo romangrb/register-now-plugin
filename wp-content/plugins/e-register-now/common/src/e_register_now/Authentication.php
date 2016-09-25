e53df6c8223e2b8a7cf2d497da8eb9fa3468c8f3
Roman <grabarroman@gmail.com>
2016-09-25 19:45:44 +0000

    add bootstrap v3

--- wp-content/plugins/e-register-now/common/src/e_register_now/Authentication.php
+++ wp-content/plugins/e-register-now/common/src/e_register_now/Authentication.php
@@ -80,7 +80,7 @@ if ( ! class_exists( 'E_Register_Now__Authentication' ) ) {
 		 * Enqueue the styles and script
 		 */
 		public function enqueue() {
-			
+			/*wp_enqueue_script( 'jquery-form-validator' );*/
 			wp_enqueue_script( 'app-authentication', e_rn_resource_url('app-authentication.js', false, 'common' ), array(), apply_filters( 'e_rn_events_js_version', E_Register_Now__Main::VERSION ) );
 			wp_enqueue_script( 'app-authentication', e_rn_resource_url('app-authentication.js', false, 'common' ), array(), apply_filters( 'e_rn_events_js_version', E_Register_Now__Main::VERSION ) );
 			wp_enqueue_style( 'app-authentication', e_rn_resource_url( 'app-authentication.css', false, 'common' ), array(), apply_filters( 'e_rn_events_css_version', E_Register_Now__Main::VERSION ) );
